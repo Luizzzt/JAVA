80f6acec72dd25da639182efec722968220439f6
Luizzzt <fernanodluiz@gmail.com>
2024-05-09 16:08:55 -0300

    Celsius To Fahrenheit
    Programa básico que transforma 29 graus em Fahrenheit.
    
    Basic Program that convert 29 degrees in to Fahrenheit

---
+++ CelsiusToFahrenheit.java
@@ -0,0 +1,7 @@
+public class CelsiusToFahrenheit {
+    public static void main(String[] args){
+        int temperatura = 29;
+        double conversao = (double) (temperatura * 1.8)+32;
+        System.out.println(conversao);
+    }
+}
